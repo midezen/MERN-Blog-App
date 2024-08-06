164d1bd5f970418f984b88e586b7d0753fa808ab
midezen <victordiya90@gmail.com>
2024-08-06 12:06:49 +0100

    created createPost route

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,40 @@
 import express from "express";
 import cors from "cors";
+import Post from "./models/Post";
 
 const app = express();
+const corsOptions = {
+  origin: "localhost:5173",
+};
 
 //! PORT
 const PORT = 5000;
+
+//* Middlewares
+app.use(express.json());
+app.use(cors(corsOptions));
+
+// ! Create Post
+app.post("/api/v1/posts/create", async (req, res) => {
+  try {
+    const postData = req.body;
+    const postCreated = await Post.create(postData);
+    res.json({
+      status: "success",
+      message: "Post created successfully",
+      postCreated,
+    });
+  } catch (error) {
+    res.json({ error });
+  }
+});
+// ! List Posts
+
+// ! Update Post
+
+// ! Get Post
+
+// ! Delete Post
 
 //! Start the server
 app.listen(PORT, console.log("server is running"));
--- backend/models/Post.js
+++ backend/models/Post.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const postSchema = new mongoose.Schema(
   {
+    title: { type: String, required: true, trim: true },
     description: { type: String, required: true, trim: true },
     image: { type: Object },
     author: {
